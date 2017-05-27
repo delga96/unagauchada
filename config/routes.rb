090846a2c2c4998f5da7dba256fb3d8ced6e4830
alejoFinocchi <alejo.finocchi@hotmail.com>
2017-05-26 21:51:36 -0300

    Comienzo de ruteo

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
 
-  get '/user/perfil'  => 'user#perfil'
-  get '/user/registrarse'  => 'user#registrarse'
+  get '/perfil'  => 'user#perfil'
+  get '/registrarse'  => 'user#registrarse'
   post '/user/registrarse' => 'user#create'
-
   get '/publicar' => 'publicar#publicar_favor'
-  get 'register/registrar'
-  get 'inicio/index'
+  get '/inicio' =>  'inicio#index'
   root 'inicio#index'
-
-  get 'login/login'
+  get '/login'  => 'login#login'
 	
 resources :registrarse
 resources :register
