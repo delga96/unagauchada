f9447042d931e07e61601db4ef0cd7b001e8c2fe
alejoFinocchi <alejo.finocchi@hotmail.com>
2017-05-29 21:35:27 -0300

    falta imagen

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529042621) do
+ActiveRecord::Schema.define(version: 20170529212137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,10 @@ ActiveRecord::Schema.define(version: 20170529042621) do
     t.string   "nombre"
     t.string   "apellido"
     t.integer  "telefono"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["apellido"], name: "index_users_on_apellido", using: :btree
